b86e3390d410a6ba27d2365a463ec6b6fc25c8a6
nelli04 <nelli_ivanova04@mail.ru>
2022-11-29 19:37:27 +0300

    es6

---
+++ Handle a Rejected Promise with catch.js
@@ -0,0 +1,16 @@
+const makeServerRequest = new Promise((resolve, reject) => { makeServerRequest.catch(error => {
+    console.log(error);
+  })
+    // responseFromServer is set to false to represent an unsuccessful response from a server
+    let responseFromServer = false;
+      
+    if(responseFromServer) {
+      resolve("We got the data");
+    } else {  
+      reject("Data not received");
+    }
+  });
+  
+  makeServerRequest.then(result => {
+    console.log(result);
+  });
